f8b891482430c811486d0027568abcbac446ae7c
Luke Rohde <lukerohde@gmail.com>
2024-12-17 19:21:02 +1100

    fix missing logger

--- app/ourmenu/account_adapter.py
+++ app/ourmenu/account_adapter.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from allauth.account.adapter import DefaultAccountAdapter
+import logging
+logger = logging.getLogger(__name__)
 
 class InviteOnlyAccountAdapter(DefaultAccountAdapter):
 
@@ -8,6 +10,7 @@ class InviteOnlyAccountAdapter(DefaultAccountAdapter):
         return 'joining_shareable_link' in request.session
         
     def get_signup_redirect_url(self, request):
+        # TODO GET THIS WORKING!  Given the session goes from unauthenticated to authenticated we a custom sign up view
         joining_link = request.session.get('joining_shareable_link')
         logger.debug(f"get_signup_redirect_url: joining_shareable_link = {joining_link}")
         if joining_link:
